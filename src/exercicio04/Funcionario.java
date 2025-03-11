c1b03a61e07e945e8f1985949905bcfc3468d0fe
Anna Julia Bobrzyk Gonzales <annajulia.bg319@gmail.com>
2025-03-11 19:40:00 -0300

    enviando os arquivos

--- src/exercicio04/Funcionario.java
+++ src/exercicio04/Funcionario.java
@@ -1,16 +1,30 @@
 package exercicio04;
 
 public class Funcionario {
-    //atributos
+    // atributos ou variáveis de instância
     String nome;
     String cargo;
     double salario;
 
-    //parametro para valor percentual de aumento
-    public double aumentarsalario(){
-        double aumento =0;
-        
-        return aumento;
+    // construtor para inicializar os atributos com valores recebidos por parâmetro
+    public Funcionario(String nome, String cargo, double salario) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
     }
 
+    // método para aplicar um aumento no salário
+    public void aumentarSalario(double porcentagem) {
+        salario = salario + (salario * porcentagem / 100);
+    }
+
+    public void aumentarSalario(String cargo, double porcentagem) {
+        if(this.cargo.equalsIgnoreCase(cargo)) {
+            salario = salario + (salario * porcentagem / 100);
+        }
+    }
+
+    public void promover(String cargo) {
+        this.cargo = cargo;
+    }
 }
--- src/exercicio04/Main.java
+++ src/exercicio04/Main.java
@@ -1,22 +1,36 @@
 package exercicio04;
 
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        DecimalFormat fMoeda = new DecimalFormat("0.00");
-        Scanner e = new Scanner(System.in);
-        Funcionario f = new Funcionario();
+        Scanner sc = new Scanner(System.in);
+        Funcionario f;
+        String nome, cargo;
+        double salario, porcentagem;
 
-        System.out.print("Nome:");
-        f.nome = e.nextLine();
-        System.out.print("Cargo:");
-        f.cargo = e.next();
-        System.out.println("Salário:");
-        f.salario = e.nextDouble();
+        System.out.print("Nome do funcionário: ");
+        nome = sc.next();
+        System.out.print("Cargo do funcionário: ");
+        cargo = sc.next();
+        System.out.print("Salário do funcionário: ");
+        salario = sc.nextDouble();
 
+        f = new Funcionario(nome, cargo, salario);
 
+        // aumentar o salario de acordo com uma porcentagem
+        System.out.println("Digite a porcentagem de aumento (0 - 100): ");
+        porcentagem = sc.nextDouble();
+        f.aumentarSalario(porcentagem);
+        System.out.println("Novo salário R$ " + f.salario);
 
+        // aumentar o salário de acordo com o cargo e uma porcentagem
+        System.out.println();
+        System.out.println("Digite a porcentagem de aumento (0 - 100): ");
+        porcentagem = sc.nextDouble();
+        System.out.println("Digite o cargo que terá aumento: ");
+        cargo = sc.next();
+        f.aumentarSalario(cargo, porcentagem);
+        System.out.println("Novo salário R$ " + f.salario);
     }
 }
